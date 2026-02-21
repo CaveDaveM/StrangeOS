4126ac954100c1d5269836b480a2867368d3ce45
CaveDaveM <149399428+CaveDaveM@users.noreply.github.com>
2026-02-21 10:46:10 +0000

    not stylised main menu

---
+++ Source/StrangeOS/Public/GameMode/BP_MainMenuGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BP_MainMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRANGEOS_API ABP_MainMenuGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
