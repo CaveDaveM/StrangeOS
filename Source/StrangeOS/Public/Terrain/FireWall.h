9e2b713bfc6992d1e8cddbf2b48e3e37261e4c13
CaveDaveM <149399428+CaveDaveM@users.noreply.github.com>
2026-02-21 15:05:19 +0000

    Mainmenu,Bugfixed,effects

--- Source/StrangeOS/Public/Terrain/FireWall.h
+++ Source/StrangeOS/Public/Terrain/FireWall.h
@@ -40,6 +40,7 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	USphereComponent* OverlapComponent;
 	
+	
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
